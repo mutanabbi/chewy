96fe955bbe4553c1f28fa546b29d70ddb5f885cb
mutanabbi <ravishankar@mail.ru>
2013-04-06 22:10:07 +0400

    Version is hashable

--- chewy/version.py
+++ chewy/version.py
@@ -30,10 +30,19 @@ class Version(object):
         if not (self.major and self.minor):
             raise VersionError('Incorrect version string: {}'.format(version))
 
+        if self.major > 9999:
+            raise VersionError('Major number too big: {}'.format(self.major))
+
+        if self.minor > 9999:
+            raise VersionError('Minor number too big: {}'.format(self.minor))
+
 
     def __lt__(self, other):
         return  self.major < other.major or (self.major == other.major and self.minor < other.minor)
 
     def __eq__(self, other):
         return (self.major, self.minor) == (other.major, other.minor)
+
+    def __hash__(self):
+        return self.major * 10000 + self.minor
 
